b4f3706a09792484bf6792c747ad074fddda3a62
Dheeraj791 <57035726+Dheeraj791@users.noreply.github.com>
2023-01-12 20:14:47 +0530

    Update cfg_clf_baseline.py

--- configs/cfg_clf_baseline.py
+++ configs/cfg_clf_baseline.py
@@ -36,10 +36,10 @@ from default_config import basic_cfg
 cfg = basic_cfg
 
 # data
-cfg.data_df = "train_df_4folds.csv"
+cfg.data_df = "/kaggle/working/rsna_breast_monai_solution/train_df_4folds.csv"
 # 1024 dataset is from:
 # https://www.kaggle.com/code/theoviel/dicom-resized-png-jpg
-cfg.root_dir = "/raid/rsna/train_images_png_kaggle"
+cfg.root_dir = "/kaggle/input/dicom-resized-png-jpg"
 # train
 cfg.train = True
 cfg.eval = True
